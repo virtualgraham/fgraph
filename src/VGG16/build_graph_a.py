94148cb6c1a6f71ed3cf95d31aa708c28bedf017
Graham Shapiro <virtualgraham@gmail.com>
2020-08-16 18:21:57 +0800

    .

--- src/VGG16/build_graph_a.py
+++ src/VGG16/build_graph_a.py
@@ -3,9 +3,9 @@ window_size = 32
 playback_random_walk_length = 10
 
 video_path = "../../media/Tabletop Objects/videos"
-db_path = "../../data/table_top_objects_10.db"
-patch_dir = "../../patches/table_top_objects_10"
+db_path = "../../data/table_top_objects_1.db"
+patch_dir = "../../patches/table_top_objects_1"
 
-from vgg16_window_walker_lib_b import build_graph
+from vgg16_window_walker_lib_b_time import build_graph
 
-build_graph(db_path, video_path, patch_dir, stride=stride, window_size=window_size, max_files=10)
+build_graph(db_path, video_path, patch_dir, stride=stride, window_size=window_size, max_files=1)
