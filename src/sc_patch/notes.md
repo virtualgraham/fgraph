d31a6253ea2fbc391d3e8e7c2d570ce83abeef86
Graham Shapiro <virtualgraham@gmail.com>
2020-10-24 18:04:38 +0800

    .

---
+++ src/sc_patch/notes.md
@@ -0,0 +1,12 @@
+g4dn.2xlarge 
+Deep Learning AMI (Ubuntu 18.04) 
+500gb EBS
+
+pip install jupyterlab
+jupyter notebook password
+
+sudo apt install transmission-cli
+
+https://academictorrents.com/collection/imagenet
+
+transmission-cli https://academictorrents.com/download/a306397ccf9c2ead27155983c254227c0fd938e2.torrent
---
+++ src/sc_patch/sc_patch.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+from torchvision import models, datasets
+ 
+import torchvision
+from torchvision import transforms
+from torchvision import models
+ 
+import torch.nn.functional as F
+import torchvision.transforms.functional as TF
+ 
+from PIL import Image
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+import random
+import time
+
+from tqdm import tqdm # progress bar
+
+import skimage
+from skimage import img_as_ubyte, img_as_float32
+
+from sklearn.model_selection import StratifiedShuffleSplit
+
+from glob import glob
+
+
+# import albumentations
+#import nibabel as nib
+#import pandas as pd
